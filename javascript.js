9a2d37ae2aea16871b0b94efb73fc9596d09320d
AdinDorf <adin@dorffamily.com>
2024-07-05 19:41:25 -0500

    Completed js function basics

--- javascript.js
+++ javascript.js
@@ -1,18 +1,42 @@
-console.log("Odin Project Practice Below:")
 
-let a = 10
-let b = 7 * a
-const MAX = 57
-let actual = MAX - 13
-let percentage = actual / MAX
 
-console.log("1: ")
-console.log(23 + 97)
-console.log("2: ")
-console.log(1 + 2 + 3 + 4 + 5 + 6)
-console.log("3: ")
-console.log ((4 + 6 + 9) / 77)
-console.log("4: ")
-console.log (a)
-console.log (9*a)
-console.log(b)
+
+function ex1() {
+    let a = 10
+    let b = 7 * a
+    const MAX = 57
+    let actual = MAX - 13
+    let percentage = actual / MAX
+    
+    console.log("1: ")
+    console.log(23 + 97)
+    console.log("2: ")
+    console.log(1 + 2 + 3 + 4 + 5 + 6)
+    console.log("3: ")
+    console.log ((4 + 6 + 9) / 77)
+    console.log("4: ")
+    console.log (a)
+    console.log (9*a)
+    console.log(b)
+    
+}
+
+function add7(num)
+{
+    return num + 7
+}
+
+function multiply(num1, num2)
+{
+    return num1*num2
+}
+
+function capitalize(str)
+{
+    return str.toUpperCase()
+}
+
+function lastLetter(str)
+{
+    return str.at(str.length - 1)
+}
